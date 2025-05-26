8dd4b9c08fe4620e41877ac0bf580fe5c1e50da3
KushalJetty <kushaljetty1203@gmail.com>
2025-05-26 15:02:48 +0530

    Changes

---
+++ generated_tests/3/recorded/content.py
@@ -0,0 +1,19 @@
+from playwright.async_api import async_playwright
+import asyncio
+
+async def test_recorded_actions():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await page.goto('https://test.teamstreamz.com/')
+        await page.click('#Username')
+        await page.fill('#Username', 'kushal.jetty@teamstreamz.com')
+        await page.fill('#Password', 'Test@123')
+        await page.click('.btn.signin-btn.bg-primary.ng-tns-c813547932-0')
+        await page.click('#Password')
+        await page.fill('#Password', 'Test@123')
+        await page.click('.btn.signin-btn.bg-primary.ng-tns-c813547932-0')
+        await page.click('.list-group-item.inactive-link.ng-star-inserted')
+        await page.click('span')
+        await browser.close()
+asyncio.run(test_recorded_actions())
